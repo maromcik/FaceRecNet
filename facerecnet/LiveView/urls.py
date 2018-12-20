b166d58f07e6e5e8c83b0b1fa73efccf14022a01
maromcik <roman.mariancik@gmail.com>
2018-12-20 10:00:56 +0100

    initial commit

--- facerecnet/LiveView/urls.py
+++ facerecnet/LiveView/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'LiveView'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
     path('run', views.runfacerec, name='run'),
 ]
