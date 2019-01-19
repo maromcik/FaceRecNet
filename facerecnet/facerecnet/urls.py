60e6d42cca51ced2d34e17731f406a42b574e779
maromcik <roman.mariancik@gmail.com>
2019-01-19 19:22:01 +0100

    initial commit

--- facerecnet/facerecnet/urls.py
+++ facerecnet/facerecnet/urls.py
@@ -20,7 +20,7 @@ from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
-from .views import home, subscribe, unsubscribe
+from .views import home, subscribe, unsubscribe, notifikacia
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,7 +31,7 @@ urlpatterns = [
     path('unsubscribe', unsubscribe, name='unsubscribe'),
     path('webpush/', include('webpush.urls')),
     path('sw.js', TemplateView.as_view(template_name='sw.js', content_type='application/x-javascript')),
-    # path('notifikacia', notifikacia, name='notifikacia'),
+    path('notifikacia', notifikacia, name='notifikacia'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- facerecnet/facerecnet/views.py
+++ facerecnet/facerecnet/views.py
@@ -50,18 +50,18 @@ def unsubscribe(request):
     return render(request, 'home.html', {user: user, 'subscription': subscription,'running': running})
 
 
-# @login_required(login_url='/accounts/login')
-# def notifikacia(request):
-#     for subscriber in Subscriber.objects.all():
-#         if subscriber.subscription:
-#             user = subscriber.user
-#             print(subscriber.user)
-#             print(subscriber.subscription)
-#             payload = {'head': 'ring', 'body': 'someone is ringing'}
-#             try:
-#                 send_user_notification(user=user, payload=payload, ttl=1000)
-#             except TypeError:
-#                 print("push typerror")
-#         else:
-#             print("no user")
-#     return HttpResponseRedirect('../')
+@login_required(login_url='/accounts/login')
+def notifikacia(request):
+    for subscriber in Subscriber.objects.all():
+        if subscriber.subscription:
+            user = subscriber.user
+            print(subscriber.user)
+            print(subscriber.subscription)
+            payload = {'head': 'ring', 'body': 'someone is ringing'}
+            try:
+                send_user_notification(user=user, payload=payload, ttl=1000)
+            except TypeError:
+                print("push typerror")
+        else:
+            print("no user")
+    return HttpResponseRedirect('../')
